f8d47295a6457636006e5ecfcfb65c88e615bba5
ha yeon <57000168+yeon0926@users.noreply.github.com>
2019-11-26 19:25:14 +0900

    블루투스 데이터 전송2
    전원 on off 버튼

--- embedded/mini_nov26a/mini_nov26a.ino
+++ embedded/mini_nov26a/mini_nov26a.ino
@@ -2,9 +2,11 @@
 #include<stdlib.h>
 
 SoftwareSerial btSerial(2,3);
+int button = 5;
 
 void setup()
 {
+  pinMode(button, INPUT_PULLUP);
   Serial.begin(9600);
   btSerial.begin(9600);
 }
@@ -13,26 +15,29 @@ int i = 80;
 
 void loop()
 {
-  int p_m_data = rand() % 4;
-  if((rand() % 3) != 0)
-  {
-    i += p_m_data;  
+  int flag = 1;
+  if(digitalRead(button)==LOW){
+    flag++;
+    btSerial.println(flag);
+    while(digitalRead(button)==LOW);
   }
-  else
-  {
-    i -= p_m_data;
+  if (flag%2 == 1) {
+    int p_m_data = rand() % 4;
+    if((rand() % 3) != 0)
+    {
+      i += p_m_data;  
+    }
+    else
+    {
+      i -= p_m_data;
+    }
+    btSerial.println(i);
+  
+    delay(1000); 
   }
-  btSerial.println(i);
-//  
-//    if (btSerial.available()) {
-//      char a = btSerial.read();
-//      if( a == 'a')
-//      {
-////        Serial.println('Equal');
-//        btSerial.println("Apple");
-//      }
-////        btSerial.print('Check A');
-//    Serial.write(a);
-//}
-  delay(1000);
+  else if (flag%2 == 0){
+    while(digitalRead(button)==HIGH);
+    flag++;
+    while(digitalRead(button)==LOW);
+  }
 }
